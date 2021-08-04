efb253824fe1ec50966e673fecec861350de41ac
Hiphop602 <74136406+Hiphop602@users.noreply.github.com>
2021-08-04 14:05:51 +0530

    Update NotifierExample.py

--- NotifierExample.py
+++ NotifierExample.py
@@ -1,1 +1,8 @@
+from plyer import notification
+
+notification.notify(
+    title = "Notification!", #Put whatever u want
+    message = "Text", #Put whatever u want
+    timeout = 5 #Basically timeout is for how long the notification will stay before it disappears 
+)
 
